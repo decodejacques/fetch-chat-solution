74e7e621a8ecbf83498d3378122500d4c82bbf7d
Jacques Le Normand <jacqueslenormand@gmail.com>
2019-10-19 09:58:22 -0400

    initial commit

---
+++ public/app.js
@@ -0,0 +1,14 @@
+let fetchAndUpdate = async () => {
+    let response = await fetch('/messages')
+    let responseBody = await response.text()
+    let parsed = JSON.parse(responseBody)
+    let msgListUL = document.getElementById('msg-list')
+    msgListUL.innerHTML = ""
+    parsed.forEach(elem => {
+        let li = document.createElement("li")
+        li.innerText = elem.user + ": " + elem.msg
+        msgListUL.append(li)
+    })
+}
+fetchAndUpdate()
+setInterval(fetchAndUpdate, 500) 
---
+++ server.js
@@ -0,0 +1,46 @@
+let express = require("express")
+let app = express()
+let cookieParser = require('cookie-parser')
+let multer = require("multer")
+let upload = multer()
+app.use(cookieParser())
+let passwordsAssoc = {}
+let sessions = {}
+let messages = []
+app.use('/static', express.static(__dirname + '/public'))
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/public/index.html")
+})
+app.post("/messages", upload.none(), (req, res) => {
+  console.log('POST messages body', req.body)
+  let newMessage = {
+    user: sessions[req.cookies["sid"]],
+    msg: req.body.message
+  }
+  messages.push(newMessage)
+  res.sendFile(__dirname + '/public/chat.html')
+})
+app.get("/messages", (req, res) => {
+  console.log('Sending back the messages')
+  res.send(JSON.stringify(messages))
+})
+app.post("/signup", upload.none(), (req, res) => {
+  let username = req.body.username
+  let password = req.body.password
+  passwordsAssoc[username] = password
+  res.send("<html><body> signup successful </body></html>")
+})
+app.post("/login", upload.none(), (req, res) => {
+  let username = req.body.username
+  let passwordGiven = req.body.password
+  let expectedPassword = passwordsAssoc[username]
+  if (expectedPassword !== passwordGiven) {
+    res.send("<html><body> invalid username or password </body></html>")
+    return
+  }
+  let sid = Math.floor(Math.random() * 10000000)
+  sessions[sid] = username
+  res.cookie('sid', sid)
+  res.sendFile(__dirname + '/public/chat.html')
+})
+app.listen(4000) 
